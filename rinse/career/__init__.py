40ae3d240b25600d392c9c81974da7841efa0d84
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-07-31 22:48:45 +0300

    feat: add Career RINSE public API

---
+++ rinse/career/__init__.py
@@ -0,0 +1,23 @@
+"""Career RINSE public API."""
+
+from .pipeline import (
+    build_contact_queue,
+    build_portfolio_cases,
+    classify_evidence,
+    derive_career_interpretations,
+    normalize_career_event,
+    redact_sensitive_text,
+    redact_url,
+    run_career_rinse,
+)
+
+__all__ = [
+    "normalize_career_event",
+    "classify_evidence",
+    "derive_career_interpretations",
+    "build_contact_queue",
+    "build_portfolio_cases",
+    "redact_sensitive_text",
+    "redact_url",
+    "run_career_rinse",
+]

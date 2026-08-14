4c0001a5391423fabc187885fcf3ed0009b32fae
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-08-14 17:13:12 +0700

    feat(rinse): export durable ProofPath source adapter

--- rinse/adapters/__init__.py
+++ rinse/adapters/__init__.py
@@ -5,6 +5,12 @@ from .kairos_liminal_receipt import (
     derive_trace_reflection_loop,
     validate_kairos_liminal_receipt,
 )
+from .liminaldb_durable_proof import (
+    DurableProofSourceError,
+    build_durable_source_trace,
+    derive_durable_proof_reflection,
+    validate_durable_bundle,
+)
 from .ttrace_jsonl import TTraceJsonLinesSource
 
 __all__ = [
@@ -12,4 +18,8 @@ __all__ = [
     "KairosLiminalReceiptError",
     "validate_kairos_liminal_receipt",
     "derive_trace_reflection_loop",
+    "DurableProofSourceError",
+    "validate_durable_bundle",
+    "build_durable_source_trace",
+    "derive_durable_proof_reflection",
 ]
